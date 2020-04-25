5e7909f9acc0eaf1975c37fd423a0e26c4b39683
owaiss22 <owaisiqbalhussain@gmail.com>
2020-04-25 17:35:49 +0500

    Updates

---
+++ precache-manifest.88f2c49bf75c9f7240498cb8571785c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1071e77a3e09065cfd7a8772f70f04f4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a3099eb7adcff9381009",
+    "url": "/monsters-rolodex/static/css/main.4a6ec692.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3099eb7adcff9381009",
+    "url": "/monsters-rolodex/static/js/main.d7f3af3c.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
